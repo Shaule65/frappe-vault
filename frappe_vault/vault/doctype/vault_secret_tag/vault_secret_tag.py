931744a24c03a55d240380ceda5cde06afbfb088
Cy-Yaksh <chandayogesh123@gmail.com>
2026-07-22 11:31:45 +0530

    refactor: remove Vault Tag and Vault Secret Tag

--- frappe_vault/vault/doctype/vault_secret_tag/vault_secret_tag.py
+++
@@ -1,7 +0,0 @@
-"""Vault Secret Tag child table controller."""
-
-from frappe.model.document import Document
-
-
-class VaultSecretTag(Document):
-    pass
--- frappe_vault/vault/doctype/vault_tag/vault_tag.py
+++
@@ -1,7 +0,0 @@
-"""Vault Tag DocType controller."""
-
-from frappe.model.document import Document
-
-
-class VaultTag(Document):
-    pass
