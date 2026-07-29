7145144e0e387d66b0f458a8645a2660509fa82a
Cy-Yaksh <chandayogesh123@gmail.com>
2026-07-29 16:06:06 +0530

    style(icons): secret type icon sizing, colors and media background UI tweaks

--- frontend/src/components/SecretTypeIcon.vue
+++ frontend/src/components/SecretTypeIcon.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex items-center gap-2">
-    <div class="w-8 h-8 rounded-lg flex items-center justify-center shrink-0" :class="colorClass">
-      <FeatherIcon :name="iconName" class="w-4 h-4" />
+  <div class="inline-flex items-center gap-2 min-w-0">
+    <div class="size-6 rounded-full flex items-center justify-center shrink-0" :class="colorClass">
+      <FeatherIcon :name="iconName" class="size-3" />
     </div>
-    <span v-if="showLabel" class="truncate font-normal">{{ label || type }}</span>
+    <span v-if="showLabel" class="truncate font-normal text-ink-gray-9">{{ label || type }}</span>
   </div>
 </template>
 
